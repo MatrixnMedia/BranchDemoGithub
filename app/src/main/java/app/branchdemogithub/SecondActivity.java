8603e41fc01ae6a567d5e9c21049f2eb3e20e1b4
Sutapa <smartdevindiana@gmail.com>
2016-01-21 14:36:19 +0530

    Second Activity...changed.

--- app/src/main/java/app/branchdemogithub/SecondActivity.java
+++ app/src/main/java/app/branchdemogithub/SecondActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
@@ -22,6 +23,7 @@ public class SecondActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Log.e("hello","I am in 2nd activity");
             }
         });
     }
